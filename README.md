955691e072337321dca3b8cbf3f81fb630417116
gowtham_p <gowtham09.panyam@gmail.com>
2022-07-15 14:55:21 +0530

    added first.java and README.md
    first.java is about adding TWO NUMBERS

---
+++ README.md
@@ -0,0 +1,4 @@
+#First java Program
+
+This repo has the first.java. \n
+This program is about adding two numbers.
---
+++ first.java
@@ -0,0 +1,7 @@
+public class first{
+    public static void main(String args[])
+    {
+        int a=5,b=6;
+        System.out.println(a+b);
+    }
+}
